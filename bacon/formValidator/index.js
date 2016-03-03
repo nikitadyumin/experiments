f38598bde44153388be64fcafced0c04eadafbb9
ndyumin <nikita.dyumin@gmail.com>
2016-03-03 20:05:27 +0500

    validatorFunction is replaced by an array of validator functions (all should return exactly `true` for the value to be valid)

--- bacon/formValidator/index.js
+++ bacon/formValidator/index.js
@@ -1,15 +1,13 @@
 $(function () {
-    const createValidators = validators =>
+    const createValidators = validations =>
         Bacon.combineWith((...props) => props.every(x => x === true),
-            Array.from(validators).map(([fieldSelector,validatorFunc]) =>
-                Bacon.$.textFieldValue($(fieldSelector)).map(validatorFunc)
+            Array.from(validations).map(([fieldSelector,validators]) =>
+                Bacon.$.textFieldValue($(fieldSelector)).map(val => validators.every(fn => fn(val) === true))
             ));
 
     const validators = new Map();
-    validators.set('#name input', x => {
-        return x.length > 0
-    });
-    validators.set('#age input', x => x.length > 0);
+    validators.set('#name input', [x => x.length > 0]);
+    validators.set('#age input', [x => !isNaN(+x), x => x > 0]);
 
     const validatorProp = createValidators(validators);
 
