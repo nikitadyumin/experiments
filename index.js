4291f814c3ce34ff53889e5e26c8cc76e1026d6b
akirsanov <Alexander.Kirsanov@reltio.com>
2016-03-03 18:57:17 +0100

    make more es6

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const result = qs.parse(document.location.search.substring(1));
 window.render = (html) => document.getElementById("playground").innerHTML = html;
 System.paths['__example__'] = result.folder;
 
-System.import('__example__').catch(function (errorMessage) {
+System.import('__example__').catch((errorMessage) => {
     document.body.innerHTML = errorMessage;
-}).then(function () {
 });
