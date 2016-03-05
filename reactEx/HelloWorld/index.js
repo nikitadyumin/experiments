38ae9e413064a37f87bea830720a59a11f56b415
akirsanov <Alexander.Kirsanov@reltio.com>
2016-03-05 19:17:39 +0100

    improvements in hello world app

--- reactEx/HelloWorld/index.js
+++ reactEx/HelloWorld/index.js
@@ -1,10 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-let HelloMessage = class extends React.Component {
-    render() {
-        return <div>Hello {this.props.name}!</div>;
-    }
+let HelloMessage = (props) => {
+    return ( <div>Hello {props.name}!</div>);
 };
 
 ReactDOM.render(<HelloMessage name='World'/>, document.getElementById('playground'));
