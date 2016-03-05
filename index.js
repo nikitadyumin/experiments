d9636d9ed8ee7c7c297b0f469aebded0a3593a23
ndyumin <nikita.dyumin@gmail.com>
2016-03-05 08:15:53 +0500

    add support for flow type annotations

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const qs = require('qs');
 const result = qs.parse(document.location.search.substring(1));
-window.render = (html) => document.getElementById("playground").innerHTML = html;
+
+System.config({
+    transpiler: "babel"
+});
+
 System.paths['__example__'] = result.folder;
 
 System.import('__example__').catch((errorMessage) => {
